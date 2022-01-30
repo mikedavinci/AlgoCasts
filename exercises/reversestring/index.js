bffb5f2971ae4c6fb326ea5b5a768ddbfb97bf2e
Miguel Esparza Jr <mike@mikedavinci.com>
2022-01-29 18:10:45 -0600

    Update index.js

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,32 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // APPROACH ONE
+  // const arr = str.split("");
+  // arr.reverse();
+  // return arr.join("");
+
+  // APPROACH TWO
+  // return str.split("").reverse().join("");
+
+  // APPROACH THREE MORE MANUAL
+  let reversed = "";
+
+  for (let character of str) {
+    reversed = character + reversed;
+    debugger;
+  }
+
+  return reversed;
+
+  // APPROACH FOUR
+  // debugger;
+  // return str.split("").reduce((reversed, character) => {
+  //   return character + reversed;
+  // }, "");
+}
+
+reverse("asdf");
 
 module.exports = reverse;
